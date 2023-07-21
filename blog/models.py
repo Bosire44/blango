75a40751272fecc737a93efcfc29428b27e39341
Caleb Captain <caleb.bosire19@students.dkut.ac.ke>
2023-07-21 14:41:30 +0000

    Finish related fields

--- blog/models.py
+++ blog/models.py
@@ -3,15 +3,16 @@ from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericRelation
+from blango_auth.models import User
 
 class Tag(models.Model):
-  value = models.TextField(max_length=100)
+  value = models.TextField(max_length=100, unique=True)
 
   def __str__(self):
     return self.value
 
 class Comment(models.Model):
-  creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  creator = models.ForeignKey(User, on_delete=models.CASCADE)
   content = models.TextField()
   content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
   object_id = models.PositiveIntegerField(db_index=True)
@@ -20,7 +21,7 @@ class Comment(models.Model):
   modified_at= models.DateTimeField(auto_now=True)
 
 class Post(models.Model):
-  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+  author = models.ForeignKey(User, on_delete=models.PROTECT)
   created_at = models.DateTimeField(auto_now_add=True)
   modified_at = models.DateTimeField(auto_now=True)
   published_at = models.DateTimeField(blank=True, null=True, db_index=True)
@@ -35,7 +36,7 @@ class Post(models.Model):
     return self.title
 
 class AuthorProfile(models.Model):
-  user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
   bio = models.TextField()
 
   def __str__(self):
